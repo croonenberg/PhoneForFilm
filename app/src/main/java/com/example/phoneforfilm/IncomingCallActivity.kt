219178c1fb84e7e45c05bffb358908edd3d7c344
croonenberg <croonenberg@hotmail.com>
2025-04-13 01:01:29 +0200

    Create IncomingCallActivity.kt

---
+++ app/src/main/java/com/example/phoneforfilm/IncomingCallActivity.kt
@@ -0,0 +1,43 @@
+package com.example.phoneforfilm
+
+import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class IncomingCallActivity : AppCompatActivity() {
+
+    private var ringtone: Ringtone? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_incoming_call)
+
+        val answerButton = findViewById<Button>(R.id.btnAnswer)
+        val declineButton = findViewById<Button>(R.id.btnDecline)
+
+        val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+        ringtone = RingtoneManager.getRingtone(applicationContext, notification)
+        ringtone?.play()
+
+        answerButton.setOnClickListener {
+            ringtone?.stop()
+            val intent = Intent(this, CallActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        declineButton.setOnClickListener {
+            ringtone?.stop()
+            finish()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        ringtone?.stop()
+    }
+}
