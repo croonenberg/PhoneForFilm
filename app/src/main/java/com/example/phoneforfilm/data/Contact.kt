e6ccbbb3afb8424491de128ac5a2017a36c8e4a8
croonenberg <croonenberg@hotmail.com>
2025-04-13 01:05:16 +0200

    Create Contact.kt

---
+++ app/src/main/java/com/example/phoneforfilm/data/Contact.kt
@@ -0,0 +1,11 @@
+package com.example.phoneforfilm.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Contact(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val phone: String
+)
