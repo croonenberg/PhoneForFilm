0d6062f5c4511b55a40fd780416766cb97114deb
croonenberg <croonenberg@hotmail.com>
2025-04-13 12:51:57 +0200

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// build.gradle.kts (Project)
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
