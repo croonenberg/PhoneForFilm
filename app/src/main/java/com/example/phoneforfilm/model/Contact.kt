73ba381cd2b145efda90dd42720aa392af2c1f3c
croonenberg <croonenberg@hotmail.com>
2025-04-13 02:50:39 +0200

    Update Contact.kt

--- app/src/main/java/com/example/phoneforfilm/model/Contact.kt
+++ app/src/main/java/com/example/phoneforfilm/model/Contact.kt
@@ -7,5 +7,6 @@ import androidx.room.PrimaryKey
 data class Contact(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
-    val phone: String
+    val phone: String,
+    val themeId: Int = 0 // Nieuw: 0 = Greenroom, 1 = Blue Stage, etc.
 )
