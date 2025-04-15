2afe134a9551df51cda3e4071363b288033a4de3
croonenberg <croonenberg@hotmail.com>
2025-04-15 03:16:32 +0200

    nieuw

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
+buildscript {
+    dependencies {
+        // Voeg de Kotlin plugin toe via de juiste versie
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "7.4.2" apply false
+    id("com.android.library") version "7.4.2" apply false
+    kotlin("android") version "1.8.0" apply false
 }
