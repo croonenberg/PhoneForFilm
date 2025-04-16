b62f713b649432cc0140a631a40c9119f444aca9
croonenberg <croonenberg@hotmail.com>
2025-04-16 06:11:56 +0200

    upload remote

--- app/src/main/java/com/example/phoneforfilm/data/MessageDao.kt
+++ app/src/main/java/com/example/phoneforfilm/data/MessageDao.kt
@@ -9,6 +9,5 @@ interface MessageDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(message: Message)
 
-    @Query("SELECT * FROM messages WHERE contactId = :contactId ORDER BY timestamp ASC")
-    fun getMessagesForContact(contactId: Int): LiveData<List<Message>>
+    fun getAll(): LiveData<List<Message>>
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 plugins {
     kotlin("jvm") version "1.9.10" apply false
     kotlin("kapt") version "1.9.10" apply false
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("com.android.library") version "8.6.1" apply false
 }
 
 allprojects {
