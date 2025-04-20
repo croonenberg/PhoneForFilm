ca754a806481f006340a0038269fdbb1be33b892
croonenberg <croonenberg@hotmail.com>
2025-04-20 06:44:49 +0200

    nieuwe workflow v4

--- app/src/main/java/com/example/phoneforfilm/view/ContactListActivity.kt
+++ app/src/main/java/com/example/phoneforfilm/view/ContactListActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.phoneforfilm.data.Contact
 import com.example.phoneforfilm.databinding.ActivityContactListBinding
 import com.example.phoneforfilm.viewmodel.ContactViewModel
 
