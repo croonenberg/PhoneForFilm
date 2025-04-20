2dcf30e9de46995c33410e6f44f25d0f6e4a441f
croonenberg <croonenberg@hotmail.com>
2025-04-20 05:26:34 +0200

    nieuwe workflow v4

--- app/src/main/java/com/example/phoneforfilm/data/ConversationDao.kt
+++ app/src/main/java/com/example/phoneforfilm/data/ConversationDao.kt
@@ -1,12 +1,11 @@
 package com.example.phoneforfilm.data
 
-import androidx.lifecycle.LiveData
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
 
 @Dao
 interface ConversationDao {
-    @Query("SELECT * FROM conversations ORDER BY timestamp DESC")
-    fun getAll(): LiveData<List<Conversation>>
 
     @Insert
     suspend fun insert(conversation: Conversation): Long
