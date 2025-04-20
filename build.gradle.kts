24e4608646f0d3195193dd71d4161fd920e99098
croonenberg <croonenberg@hotmail.com>
2025-04-20 04:12:59 +0200

    nieuwe workflow v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,13 @@
 plugins {
     // Android Gradle Plugin versions (apply false to defer to subprojects)
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
     // Kotlin Android plugin
     kotlin("android") version "1.9.0" apply false
 }
 
 // Global clean task to delete the build directory of the root project
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
+    // Use the new buildDirectory API instead of the deprecated buildDir property
+    delete(rootProject.layout.buildDirectory)
 }
