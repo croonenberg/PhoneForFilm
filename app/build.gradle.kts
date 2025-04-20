f208baec8d358f7ae9b7d38f4c56d934ac4eff6a
croonenberg <croonenberg@hotmail.com>
2025-04-20 04:05:56 +0200

    nieuwe workflow v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,15 +32,15 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     // Room
     implementation("androidx.room:room-runtime:2.7.0")
-    implementation("androidx.room:room-ktx:2.5.2")
+    implementation("androidx.room:room-ktx:2.7.0")
 
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
 
     // RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.4.0")
