abc012e95afda66cf1f5161016e65dfa67ab15cd
croonenberg <croonenberg@hotmail.com>
2025-04-20 04:29:21 +0200

    nieuwe workflow v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.example.phoneforfilm"
         minSdk = 21
-
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
