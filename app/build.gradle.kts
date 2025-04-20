ab4d4c877cff0de48765694faaa6432e8172a045
croonenberg <croonenberg@hotmail.com>
2025-04-20 04:31:14 +0200

    nieuwe workflow v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
 
     // Room
     implementation("androidx.room:room-runtime:2.7.0")
-    implementation("androidx.room:room-compiler:2.7.0")
+    annotationProcessor("androidx.room:room-compiler:2.7.0")
     implementation("androidx.room:room-ktx:2.7.0")
 
     // Lifecycle
