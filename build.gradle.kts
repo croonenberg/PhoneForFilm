c3909756c48c62cb19a79d16439ca9da83f090df
croonenberg <croonenberg@hotmail.com>
2025-04-20 04:09:25 +0200

    nieuwe workflow v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Android Gradle Plugin versions (apply false to defer to subprojects)
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
     // Kotlin Android plugin
     kotlin("android") version "1.9.0" apply false
 }
