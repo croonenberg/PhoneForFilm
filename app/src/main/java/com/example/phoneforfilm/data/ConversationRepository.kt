0b2e521fdecfa57755fabcbfe70d855a77517c77
croonenberg <croonenberg@hotmail.com>
2025-04-20 05:30:54 +0200

    nieuwe workflow v4

--- app/src/main/java/com/example/phoneforfilm/data/ConversationRepository.kt
+++ app/src/main/java/com/example/phoneforfilm/data/ConversationRepository.kt
@@ -2,13 +2,13 @@ package com.example.phoneforfilm.data
 
 import androidx.lifecycle.LiveData
 
-@Suppress("unused")
 class ConversationRepository(private val dao: ConversationDao) {
-    val allConversations: LiveData<List<Conversation>> = dao.getAllConversations()
 
-    suspend fun insert(conversation: Conversation): Long = dao.insertConversation(conversation)
+    fun getAll(): LiveData<List<Conversation>> = dao.getAll()
 
-    suspend fun update(conversation: Conversation) = dao.updateConversation(conversation)
+    suspend fun insert(conversation: Conversation): Long = dao.insert(conversation)
 
-    suspend fun delete(conversation: Conversation) = dao.deleteConversation(conversation)
+    suspend fun update(conversation: Conversation) = dao.update(conversation)
+
+    suspend fun delete(conversation: Conversation) = dao.delete(conversation)
 }
