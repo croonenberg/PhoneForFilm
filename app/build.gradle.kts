dd0a796909340a0011590305f8cd2e239d1be5e3
croonenberg <croonenberg@hotmail.com>
2025-04-21 23:35:48 +0200

    nieuwe workflow v4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,6 @@ dependencies {
 
     // extra herrie
     implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.3.0")
 }
