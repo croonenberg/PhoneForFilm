253f39601a83db2dc9ef6a3707390eb1748da29e
croonenberg <croonenberg@hotmail.com>
2025-05-03 01:48:33 +0200

    v19_2.1 (BUILD SUCCESSFUL)

--- app/src/main/java/com/example/phoneforfilm/data/AppDatabase.kt
+++ app/src/main/java/com/example/phoneforfilm/data/AppDatabase.kt
@@ -27,7 +27,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "phone_for_film_database"
                 )
-                    .fallbackToDestructiveMigration()
+                    .fallbackToDestructiveMigration(true)
                     .build()
                     .also { INSTANCE = it }
             }
