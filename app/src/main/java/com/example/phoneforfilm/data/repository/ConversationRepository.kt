3033e45a4eb038c27baf4b166e67e8adf1370105
croonenberg <croonenberg@hotmail.com>
2025-05-04 06:36:53 +0200

    v19.4.0 chat werkt bijna

--- app/src/main/java/com/example/phoneforfilm/data/repository/ConversationRepository.kt
+++ app/src/main/java/com/example/phoneforfilm/data/repository/ConversationRepository.kt
@@ -2,13 +2,14 @@ package com.example.phoneforfilm.data.repository
 
 import com.example.phoneforfilm.data.Conversation
 import com.example.phoneforfilm.data.ConversationDao
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ConversationRepository @Inject constructor(
     private val dao: ConversationDao
 ) {
     suspend fun create(conversation: Conversation) = dao.insert(conversation)
-    suspend fun getAll() = dao.getAll()
+    fun getAll(): Flow<List<Conversation>> = dao.getAll()
     suspend fun update(conversation: Conversation) = dao.update(conversation)
     suspend fun getById(id: Int) = dao.getById(id)
 }
--- app/src/main/java/com/example/phoneforfilm/view/ChatActivity.kt
+++ app/src/main/java/com/example/phoneforfilm/view/ChatActivity.kt
@@ -2,6 +2,7 @@ package com.example.phoneforfilm.view
 
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.phoneforfilm.adapter.MessageAdapter
@@ -43,6 +44,77 @@ class ChatActivity : AppCompatActivity() {
     }
 
     fun onMessageLongPressed(msg: Message) {
-        // TODO: show theme/language/edit/delete options
+        val options = arrayOf(
+            getString(R.string.edit_message),
+            getString(R.string.delete_message),
+            getString(R.string.choose_theme),
+            getString(R.string.choose_language)
+        )
+        AlertDialog.Builder(this)
+            .setTitle(R.string.message_options)
+            .setItems(options) { dialog, which ->
+                when (which) {
+                    0 -> showEditDialog(msg)
+                    1 -> {
+                        viewModel.deleteMessage(msg)
+                        viewModel.getMessages(conversationId)
+                    }
+                    2 -> showThemeChooser()
+                    3 -> showLanguageChooser()
+                }
+            }
+            .show()
+    }
+
+    private fun showEditDialog(msg: Message) {
+        val input = android.widget.EditText(this).apply {
+            setText(msg.text)
+            setSelection(msg.text.length)
+        }
+        AlertDialog.Builder(this)
+            .setTitle(R.string.edit_message)
+            .setView(input)
+            .setPositiveButton(R.string.save) { _, _ ->
+                val newText = input.text.toString()
+                if (newText.isNotBlank()) {
+                    msg.text = newText
+                    viewModel.updateMessage(msg)
+                    viewModel.getMessages(conversationId)
+                }
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .show()
+    }
+
+    private fun showThemeChooser() {
+        val themeNames = arrayOf(
+            getString(R.string.theme_greenroom),
+            getString(R.string.theme_blue_stage),
+            getString(R.string.theme_grey_card),
+            getString(R.string.theme_neutral_light),
+            getString(R.string.theme_darkroom)
+        )
+        AlertDialog.Builder(this)
+            .setTitle(R.string.choose_theme)
+            .setItems(themeNames) { _, which ->
+                // TODO: apply theme choice to this chat
+            }
+            .show()
+    }
+
+    private fun showLanguageChooser() {
+        val languages = arrayOf(
+            getString(R.string.language_nl),
+            getString(R.string.language_en),
+            getString(R.string.language_de),
+            getString(R.string.language_fr),
+            getString(R.string.language_es)
+        )
+        AlertDialog.Builder(this)
+            .setTitle(R.string.choose_language)
+            .setItems(languages) { _, which ->
+                // TODO: apply language choice to this chat
+            }
+            .show()
     }
 }
