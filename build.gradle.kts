e38805e7e5c3bd29e900fb06ef265e595e0bf1d4
croonenberg <croonenberg@hotmail.com>
2025-05-05 00:44:13 +0200

    v19.6.8 chat werkt bijna

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.56.2")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.9.2" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
