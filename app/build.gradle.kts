ef5476a30cfbc600e3ed8e64698629d9bf35025c
croonenberg <croonenberg@hotmail.com>
2025-05-04 15:55:16 +0200

    v19.4.0 chat werkt bijna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,45 +43,48 @@ android {
         viewBinding = true
         dataBinding = true
     }
-}
+    repositories {
+        google()
+        mavenCentral()
+    }
 
-dependencies {
-    // AndroidX core & UI
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.3.0")
-    implementation("androidx.recyclerview:recyclerview:1.5.1")
-    implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.3")
+    dependencies {
+        // AndroidX core & UI
+        implementation("androidx.core:core-ktx:1.16.0")
+        implementation("androidx.appcompat:appcompat:1.7.0")
+        implementation("com.google.android.material:material:1.12.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+        implementation("androidx.recyclerview:recyclerview:1.4.0")
+        implementation("androidx.activity:activity-ktx:1.10.1")
+        implementation("androidx.fragment:fragment-ktx:1.8.6")
 
-    // Lifecycle (ViewModel, LiveData)
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    kapt("androidx.lifecycle:lifecycle-compiler:2.6.2")
+        // Lifecycle (ViewModel, LiveData)
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+        kapt("androidx.lifecycle:lifecycle-compiler:2.8.7")
 
-    // Coroutines & Flow
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.4")
+        // Coroutines & Flow
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
 
-    // Room (database, DAO, entities)
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
+        // Room (database, DAO, entities)
+        implementation("androidx.room:room-runtime:2.7.1")
+        implementation("androidx.room:room-ktx:2.7.1")
+        kapt("androidx.room:room-compiler:2.7.1")
 
-    // Hilt for DI
-    implementation("com.google.dagger:hilt-android:2.47")
-    kapt("com.google.dagger:hilt-android-compiler:2.47")
-    implementation("androidx.hilt:hilt-lifecycle-viewmodel:1.0.0")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+        // Hilt for DI
+        implementation("com.google.dagger:hilt-android:2.56.2")
+        kapt("com.google.dagger:hilt-android-compiler:2.56.2")
 
-    // Navigation Component (optioneel)
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.1")
+        // Navigation Component
+        implementation("androidx.navigation:navigation-fragment-ktx:2.8.9")
+        implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
 
-    // Test dependencies
-    testImplementation("junit:junit:4.14.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.6")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.0")
+        // Testing
+        testImplementation("junit:junit:4.13.2")
+        androidTestImplementation("androidx.test.ext:junit:1.2.1")
+        androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    }
+
 }
