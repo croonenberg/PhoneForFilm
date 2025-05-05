aa42fe46576b56f49b0967ef2bec59de1c36376a
croonenberg <croonenberg@hotmail.com>
2025-05-05 07:02:20 +0200

    v19.6.8 chat werkt bijna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ android {
         }
     }
 
-    // Fix voor Room warning
     packagingOptions {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -55,8 +54,13 @@ android {
     }
 }
 
-room {
-    exportSchema = false
+kapt {
+    arguments {
+        arg("room.schemaLocation", "$projectDir/schemas")
+        arg("room.incremental", "true")
+        arg("room.expandProjection", "true")
+        arg("room.exportSchema", "false")
+    }
 }
 
 dependencies {
@@ -68,8 +72,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
-    //noinspection RiskyLibrary,PlaySdkIndexDeprecated
-    implementation("com.google.android.play:core:1.10.3")
+
 
     // Room
     implementation("androidx.room:room-runtime:2.7.1")
