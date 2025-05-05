c867cb4d5c1f6967ea7ee91516fcf150c583d0ce
croonenberg <croonenberg@hotmail.com>
2025-05-05 11:28:27 +0200

    v19.6.8 chat werkt bijna

--- app/src/main/java/com/example/phoneforfilm/data/Conversation.kt
+++ app/src/main/java/com/example/phoneforfilm/data/Conversation.kt
@@ -1,20 +1,18 @@
 package com.example.phoneforfilm.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import androidx.room.ColumnInfo
 
 @Entity(tableName = "conversations")
 data class Conversation(
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
-    val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
 
     @ColumnInfo(name = "contactId")
     val contactId: Int,
 
     @ColumnInfo(name = "lastMessage")
-    val lastMessage: String,
+    val lastMessage: String = "",
 
     @ColumnInfo(name = "timestamp")
     val timestamp: Long,
