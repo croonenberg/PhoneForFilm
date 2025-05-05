168578684f91fcacf3d3755fa07ae04ce303fcd6
croonenberg <croonenberg@hotmail.com>
2025-05-05 10:20:30 +0200

    v19.6.8 chat werkt bijna

--- app/src/main/java/com/example/phoneforfilm/data/local/dao/ContactDao.kt
+++ app/src/main/java/com/example/phoneforfilm/data/local/dao/ContactDao.kt
@@ -1,17 +1,25 @@
 package com.example.phoneforfilm.data.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.example.phoneforfilm.data.model.Contact
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ContactDao {
-
     @Query("SELECT * FROM contacts ORDER BY name ASC")
-    fun getAllContacts(): Flow<List<Contact>>
+    fun getAll(): LiveData<List<Contact>>
+
+    @Query("SELECT * FROM contacts")
+    fun getAllNow(): List<Contact>
+
+    @Query("SELECT id FROM contacts WHERE androidContactId = :androidId LIMIT 1")
+    fun getIdByAndroidId(androidId: Long): Int?
+
+    @Query("SELECT * FROM contacts WHERE id = :id LIMIT 1")
+    fun getContactById(id: Int): Contact?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(contact: Contact)
+    suspend fun insert(contact: Contact): Long
 
     @Update
     suspend fun update(contact: Contact)
