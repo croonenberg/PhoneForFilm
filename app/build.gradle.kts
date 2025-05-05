8af1e39b9d150f05e4fe2a4464528d82a67f6234
croonenberg <croonenberg@hotmail.com>
2025-05-05 09:58:50 +0200

    v19.6.8 chat werkt bijna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,73 +18,74 @@ android {
         javaCompileOptions {
             annotationProcessorOptions {
                 arguments += mapOf("room.schemaLocation" to "$projectDir/schemas")
+            }
         }
-    }
 
 
-    buildFeatures {
-        viewBinding = true
-        dataBinding = true
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        buildFeatures {
+            viewBinding = true
+            dataBinding = true
         }
-    }
 
-    bundle {
-        language {
-            enableSplit = false
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
-    }
 
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        kotlinOptions {
+            jvmTarget = "17"
         }
-    }
 
-    kapt {
-        arguments {
-            arg("room.schemaLocation", "$projectDir/schemas")
-            arg("room.incremental", "true")
-            arg("room.expandProjection", "true")
+        buildTypes {
+            getByName("release") {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
         }
-    }
 
-    dependencies {
-        implementation("androidx.core:core-ktx:1.16.0")
-        implementation("androidx.appcompat:appcompat:1.7.0")
-        implementation("com.google.android.material:material:1.12.0")
-        implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-        implementation("androidx.activity:activity-ktx:1.10.1")
-        implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
+        bundle {
+            language {
+                enableSplit = false
+            }
+        }
+
+        packaging {
+            resources {
+                excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            }
+        }
+
+        kapt {
+            arguments {
+                arg("room.schemaLocation", "$projectDir/schemas")
+                arg("room.incremental", "true")
+                arg("room.expandProjection", "true")
+            }
+        }
+
+        dependencies {
+            implementation("androidx.core:core-ktx:1.16.0")
+            implementation("androidx.appcompat:appcompat:1.7.0")
+            implementation("com.google.android.material:material:1.12.0")
+            implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+            implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+            implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+            implementation("androidx.activity:activity-ktx:1.10.1")
+            implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
 
 
-        // Room
-        implementation("androidx.room:room-runtime:2.7.1")
-        kapt("androidx.room:room-compiler:2.7.1")
-        implementation("androidx.room:room-ktx:2.7.1")
+            // Room
+            implementation("androidx.room:room-runtime:2.7.1")
+            kapt("androidx.room:room-compiler:2.7.1")
+            implementation("androidx.room:room-ktx:2.7.1")
 
-        // Hilt
-        implementation("com.google.dagger:hilt-android:2.56.2")
-        kapt("com.google.dagger:hilt-compiler:2.56.2")
-        implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+            // Hilt
+            implementation("com.google.dagger:hilt-android:2.56.2")
+            kapt("com.google.dagger:hilt-compiler:2.56.2")
+            implementation("androidx.hilt:hilt-navigation-fragment:1.2.0")
+        }
     }
 }
