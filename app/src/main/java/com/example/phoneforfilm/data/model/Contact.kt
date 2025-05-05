2ae581a91914e644828ff8f272c3dc8b016d243e
croonenberg <croonenberg@hotmail.com>
2025-05-05 10:26:14 +0200

    v19.6.8 chat werkt bijna

--- app/src/main/java/com/example/phoneforfilm/data/model/Contact.kt
+++ app/src/main/java/com/example/phoneforfilm/data/model/Contact.kt
@@ -1,5 +1,6 @@
 package com.example.phoneforfilm.data.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -9,4 +10,7 @@ data class Contact(
     val id: Int = 0,
     val name: String,
     val phoneNumber: String
+    val androidContactId: Long? = null // <-- voeg deze toe
 )
+@ColumnInfo(name = "androidContactId")
+val androidContactId: Long?
