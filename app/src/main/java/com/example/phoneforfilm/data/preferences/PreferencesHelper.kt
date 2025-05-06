7ebe364d6fb15be39e71e6f45df21f9890366a52
croonenberg <croonenberg@hotmail.com>
2025-05-06 02:30:52 +0200

    v19.8.2 eindelijk wat meer chat

--- app/src/main/java/com/example/phoneforfilm/data/preferences/PreferencesHelper.kt
+++ app/src/main/java/com/example/phoneforfilm/data/preferences/PreferencesHelper.kt
@@ -9,6 +9,7 @@ import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
+
 private val Context.dataStore by preferencesDataStore(name = "settings")
 
 object PreferencesHelper {
