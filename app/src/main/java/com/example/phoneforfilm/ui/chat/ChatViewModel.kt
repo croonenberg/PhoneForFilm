c6a82b8ac9bf7f6c1baa250eade18f9025c8083e
croonenberg <croonenberg@hotmail.com>
2025-05-09 03:52:54 +0200

    v19.9.9 deep-dive 7/5

--- app/src/main/java/com/example/phoneforfilm/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/phoneforfilm/ui/chat/ChatViewModel.kt
@@ -3,7 +3,7 @@ package com.example.phoneforfilm.ui.chat
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.phoneforfilm.domain.usecase.GetConversationThemeUseCase
-import com.example.phoneforfilm.domain/usecase/SetConversationThemeUseCase
+import com.example.phoneforfilm.domain.usecase.SetConversationThemeUseCase
 import com.example.phoneforfilm.ui.common.UIState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
