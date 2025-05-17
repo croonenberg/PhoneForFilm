973934b3c59603bfd9e90803f84b3d265cd016b0
croonenberg <croonenberg@hotmail.com>
2025-05-18 00:12:21 +0200

    werkende build v19A1.3?

--- app/src/main/java/com/example/phoneforfilm/view/MainActivity.kt
+++ app/src/main/java/com/example/phoneforfilm/view/MainActivity.kt
@@ -1,34 +1,29 @@
 package com.example.phoneforfilm.view
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.phoneforfilm.adapter.ContactAdapter
 import com.example.phoneforfilm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ContactAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Toolbar instellen
         setSupportActionBar(binding.toolbar)
 
-        // Start een telefoongesprek
-            startActivity(Intent(this, CallActivity::class.java))
-        }
+        // RecyclerView initialiseren met bestaande ContactAdapter
+        adapter = ContactAdapter(emptyList())
+        binding.recyclerViewFilms.layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewFilms.adapter = adapter
 
-        // Start chat-overzicht
-            startActivity(Intent(this, ChatListActivity::class.java))
-        }
-
-        // Wijzig taal
-            startActivity(Intent(this, LanguageSelectionActivity::class.java))
-        }
-
-        // Wijzig thema
-            startActivity(Intent(this, ThemeSettingsActivity::class.java))
-        }
+        // TODO: Vul 'adapter' later met echte data vanuit ViewModel/Repository
     }
 }
