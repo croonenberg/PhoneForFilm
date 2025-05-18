8d7665ebee501e0fb11351fa1436565d837f77d2
croonenberg <croonenberg@hotmail.com>
2025-05-19 01:50:51 +0200

    feature/splash-screen_v20.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.3.0"
     id("org.jetbrains.kotlin.android") version "1.9.22"
-    id("io.gitlab.arturbosch.detekt")
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    id("com.android.application") version "8.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
 
