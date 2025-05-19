f1094fb7f17d9967eb3b318e1389549fd8d1da69
croonenberg <croonenberg@hotmail.com>
2025-05-19 03:08:10 +0200

    feature/splash-screen_v20.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") apply false
+    id("com.android.application")
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.1.21" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false
