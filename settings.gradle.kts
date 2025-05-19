6c31bcb31b31793f594f6616043f5e111fcd1b27
croonenberg <croonenberg@hotmail.com>
2025-05-19 03:10:38 +0200

    feature/splash-screen_v20.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,9 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.4.0"       // ← vul jouw AGP-versie in
-        id("org.jetbrains.kotlin.android") version "1.9.22"
-        id("org.jetbrains.kotlin.kapt")   version "1.9.22"
+        id("com.android.application") version "9.2.1"       // ← vul jouw AGP-versie in
+        id("org.jetbrains.kotlin.android") version "2.1.21"
+        id("org.jetbrains.kotlin.kapt")   version "2.1.21"
         id("com.google.dagger.hilt.android") version "2.52"
         id("io.gitlab.arturbosch.detekt")  version "1.23.3"
     }
