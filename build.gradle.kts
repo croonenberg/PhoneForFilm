77604d4d9e30d4c6b522ff0d17ffb33516c800ee
croonenberg <croonenberg@hotmail.com>
2025-05-19 02:26:47 +0200

    feature/splash-screen_v20.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("com.android.application") version "8.9.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.20" apply false
-    id("io.gitlab.arturbosch.detekt") version "1.23.3"
+    id("io.gitlab.arturbosch.detekt") version "1.23.3" apply false
     id("com.google.dagger.hilt.android") version "2.56.2" apply false // Hilt plugin
+    id("org.jetbrains.kotlin.kapt") version "2.1.20" apply false   // <- optioneel, voorkomt IDE-hint
 }
 
 detekt {
