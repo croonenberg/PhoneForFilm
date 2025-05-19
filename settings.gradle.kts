289d3e642b8c867a2d058d1a7de109a877a921dd
croonenberg <croonenberg@hotmail.com>
2025-05-19 03:04:25 +0200

    feature/splash-screen_v20.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,2 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-    plugins {
-        id("com.android.application") version "8.4.0"
-        id("org.jetbrains.kotlin.android") version "1.9.22"
-        id("com.google.dagger.hilt.android") version "2.52"
-        id("io.gitlab.arturbosch.detekt") version "1.23.3"
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 rootProject.name = "PhoneForFilm"
 include(":app")
