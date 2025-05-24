bc02ebbdefc8fb185e45b9a1f2236cda35555d29
croonenberg <croonenberg@hotmail.com>
2025-05-24 11:07:09 +0200

    feature/splash-screen_v20.0.0

--- app/src/main/java/com/example/phoneforfilm/adapter/ConversationAdapter.kt
+++ app/src/main/java/com/example/phoneforfilm/adapter/ConversationAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.phoneforfilm.data.Conversation
+import com.example.phoneforfilm.data.local.entity.Conversation
 import com.example.phoneforfilm.databinding.ItemConversationBinding
 
 class ConversationAdapter(
